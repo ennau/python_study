f99da324368381b8f31dfe9c13886d5f7375b377
ennau <ennau1@ukr.net>
2016-11-17 18:15:48 +0200

    Update random_number.py

--- random_number.py
+++ random_number.py
@@ -1,19 +1,24 @@
 # coding utf-8
 import random
 
-secret = random.randint(1, 10) 
-while True:
-	try:
-		guess = int(raw_input("?"))
-	except ValueError:
-		print "please enter number"		
-		continue
-	if secret == guess:
-		print "WELL DONE!"
-		break
-	if secret > guess:
-		print ">"
-	else:
-		print "<"
+secret = random.randint(1, 10)
 
+counter = 0
+
+while counter < 5:
+        try:
+                guess = int(raw_input("?\n"))
+        except ValueError:
+                print "please enter number"
+                continue
+        counter += 1
+        if secret == guess:
+                print "WELL DONE!"
+                break
+        if secret > guess:
+                print ">"
+        else:
+                print "<"
+else:
+        print "You are looser"
 
