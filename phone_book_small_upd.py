72227b86ca5144e7df88723dbf34b4b9d7f5373c
ennau <ennau1@ukr.net>
2016-11-26 06:55:01 -0500

    Update phone_book_small_upd.py

--- phone_book_small_upd.py
+++ phone_book_small_upd.py
@@ -1,10 +1,11 @@
 # coding utf-8
 
-import json
+# import json
+serializer = __import__('json')
 
-#phonebook['BILL'] = 911
-#phonebook['BOB'] = 912
-#print '\n'.join(["{}: {}".format(name, phone) for name, phone in d.items()])
+# phonebook['BILL'] = 911
+# phonebook['BOB'] = 912
+# print '\n'.join(["{}: {}".format(name, phone) for name, phone in d.items()])
 
 """
 #### EDIT!!!
@@ -43,37 +44,33 @@ phone = ''
 # Open file phonebook
 try:
     with open('phonebook.txt', 'rt') as gg:
-        phonebook = json.load(gg)
+        phonebook = serializer.load(gg)
     print phonebook
 except ValueError:
-    phonebook={'default': 0}
+    phonebook = {'default': 0}
     print phonebook
+
 
 # Decorator - save file
 def save_dec(f):
     def wrapper():
         res = f()
-        with open ('phonebook.txt', 'wt') as ff:
-            json.dump(phonebook, ff)
+        with open('phonebook.txt', 'wt') as ff:
+            serializer.dump(phonebook, ff)
         return res
     return wrapper
 
-# Main functions
-def read():
-    name = raw_input("Search name from dict\n")
-    if name in phonebook:
-        print "{}: phone is {}".format(name, phonebook[name])
-    else:
-        print name + ' ' + 'is not found'
 
 @save_dec
-def delete():
-    name = raw_input("Delete name from dict\n")
-    if name in phonebook:
-        del phonebook[name]
-        print "Delete {}: phone {}".format(name, phone)
-    else:
-        print name + ' ' + 'is not found'
+def create():
+    name = raw_input("Create name to dict\n")
+    try:
+        phone = int(raw_input("Add phone\n"))
+        phonebook[name] = phone
+        print phonebook
+    except ValueError:
+        print "Phone must be integer!!"
+
 
 @save_dec
 def update():
@@ -85,12 +82,24 @@ def update():
     else:
         print name + ' ' + 'is not found'
 
+
 @save_dec
-def create():
-    name = raw_input("Create name to dict\n")
-    phone = int(raw_input("Add phone\n"))
-    phonebook[name] = phone
-    print phonebook
+def delete():
+    name = raw_input("Delete name from dict\n")
+    if name in phonebook:
+        del phonebook[name]
+        print "Delete {}: phone {}".format(name, phone)
+    else:
+        print name + ' ' + 'is not found'
+
+
+def read():
+    name = raw_input("Search name from dict\n")
+    if name in phonebook:
+        print "{}: phone is {}".format(name, phonebook[name])
+    else:
+        print name + ' ' + 'is not found'
+
 
 def show():
     print phonebook
@@ -116,4 +125,3 @@ while flag != 'exit':
         print "ERROR, Please choose symbol C U D R S or exit"
 else:
     print "Done"
-
