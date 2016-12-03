bb0c13a672762d57e0451c5fa51533d68f23c269
ennau <ennau1@ukr.net>
2016-12-03 09:12:55 -0500

    Update lsfile_csv.py

--- phonebook_upd/lsfile_csv.py
+++ phonebook_upd/lsfile_csv.py
@@ -19,10 +19,10 @@ def save(phonebook):
             for row in phonebook.items():
                 w.writerow(row)
 
-"""
+'''
 with open('eggs.csv', 'wb') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
     spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
-"""
+'''
